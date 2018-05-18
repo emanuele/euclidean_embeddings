13d6e2d8f71650cad0cfb7951ef95304096e7e8d
Emanuele Olivetti <emanuele.olivetti@gmail.com>
2018-05-18 11:21:41 +0200

    added dissimilarity representation, fastmap, lmds and some code for distances

---
+++ fastmap.py
@@ -0,0 +1,79 @@
+"""Fastmap algorithm.
+
+See: Falutsos and Lin, "FastMap: a fast algorithm for indexing,
+data-mining and visualization of traditional and multimedia datasets",
+1995.
+
+"""
+
+from __future__ import print_function
+import numpy as np
+
+
+def recursive_distance2(X, idx, distance, Y):
+    """Compute the squared recursive distance between an iterable of
+    objects and a single object with index (idx), given the original
+    distance and the (partial) projection Y. Necessary for
+    Fastmap. This is a pretty fast implementation.
+    """
+    tmp1 = distance(X, np.array([X[idx]]))
+    tmp1 *= tmp1
+    tmp2 = (Y - Y[idx])
+    tmp2 *= tmp2
+    return tmp1.squeeze() - tmp2.sum(1)
+
+
+def find_pivot_points_from_X_fast(X, distance, Y):
+    """Find two points (a, b) far away, with a heuristic, from the objects
+    X given the distance function.
+    """
+    idx_r = np.random.randint(len(X))
+    idx_a = recursive_distance2(X, idx_r, distance, Y).argmax()
+    idx_b = recursive_distance2(X, idx_a, distance, Y).argmax()
+    return idx_a, idx_b
+
+
+def find_pivot_points_scalable(X, distance, Y, k, permutation=True, c=2.0):
+    """Find two points (a, b) far away, with a heuristic, from the objects
+    X given the distance function, assuming objects as clustered in k
+    clusters and subsampling X accordingly.
+    """
+    size = int(max(1, np.ceil(c * k * np.log(k))))
+    if permutation:
+        idx = np.random.permutation(len(X))[:size]
+    else:
+        idx = range(size)
+
+    tmp_a, tmp_b = find_pivot_points_from_X_fast(X[idx], distance, Y[idx])
+    return idx[tmp_a], idx[tmp_b]
+
+
+def projection_from_X(X, distance, idx_a, idx_b, Y, eps=1.0e-10):
+    """Compute projections of objects X, given a distance function, two
+    indices of pivot points (idx_a, idx_b), and their partial
+    projection Y.
+
+    """
+    tmp1 = recursive_distance2(X, idx_a, distance, Y)
+    tmp2 = tmp1[idx_b] + eps
+    tmp3 = recursive_distance2(X, idx_b, distance, Y)
+    Yj = (tmp1 + tmp2 - tmp3) / (2.0 * np.sqrt(tmp2))
+    return Yj
+
+
+def fastmap(X, distance, k, subsample=False, n_clusters=10, verbose=False):
+    """Fastmap algorithm. This is a pretty fast implementation.
+    """
+    Y = np.zeros([len(X), k])
+    for i in range(k):
+        if verbose:
+            print("Dimension %s" % i)
+
+        if subsample:
+            idx_a, idx_b = find_pivot_points_scalable(X, distance, Y, n_clusters)
+        else:
+            idx_a, idx_b = find_pivot_points_from_X_fast(X, distance, Y)
+
+        Y[:, i] = projection_from_X(X, distance, idx_a, idx_b, Y)
+
+    return Y
---
+++ lmds.py
@@ -0,0 +1,55 @@
+"""
+LMDS algorithm.
+references:
+      -https://www.sciencedirect.com/science/article/pii/S0031320308005049#sec2
+      -https://ieeexplore.ieee.org/stamp/stamp.jsp?tp=&arnumber=7214117
+      -http://www.cmap.polytechnique.fr/~peyre/cours/x2005signal/dimreduc_landmarks.pdf
+
+1. choose n landmark points
+2. compute distance-matrix Dl of landmax points, using the original distance
+3. apply MDS to landmark points to obtain Euclidean embedding of landmarks of size k
+4. compute U: the eigenvectors matrix and E: the diagonal eigenvalue matrix
+5. compute E ^ (-1/2) * transpose (U)
+6. compute median column means of Dl: mu
+7. calculate squared distances of each point from n landmark points d_i
+8.1 project points on eigenvectors to obtain embedding coordinates, use formula:
+       y_i = 0.5 * pseudoinv_transpose(M) * ( mu - d_i )
+where pseudoinv_transpose(M) is defined as : E ^ (-1/2) * transpose (U)
+8.2 possible alternative: use only a subset of eigenvectors having the biggest
+eigenvalues, dimention of embedded space in this case is n' < n
+"""
+
+import numpy as np
+from dissimilarity import compute_prototypes
+
+
+def compute_lmds(dataset, distance=None, nl=10, k=4, landmark_policy='random'):
+    """Given a dataset, computes the lMDS Euclidean embedding of size k
+    using nl landmarks. The dataset must be an array.
+    """
+    landmarks_idx = compute_prototypes(dataset, num_prototypes=nl,
+                                       distance=distance,
+                                       prototype_policy=landmark_policy)
+
+    d = distance(dataset, dataset[landmarks_idx])
+
+    # Use squared distances
+    d *= d
+    D = distance(dataset[landmarks_idx], dataset[landmarks_idx])
+    D *= D
+
+    # Compute cMDS on landmarks and get top k eigenvalues/eigenvectors
+    n = D.shape[0]
+    H = np.identity(n) - (1.0 / n) * np.ones((n, n))
+    B = -0.5 * H.dot(D).dot(H)
+    Lambda, U = np.linalg.eigh(B)  # eigh() because B (and D) must be symmetric
+    U = U.T
+    idx = Lambda.argsort()[::-1]
+    Lambda_plus = Lambda[idx][:k]
+    U_plus = U[idx][:k]
+
+    # Compute M_sharp and the embedding:
+    M_sharp = np.diag(1.0 / np.sqrt(Lambda_plus)).dot(U_plus)
+    Y = (0.5 * M_sharp.dot((D.mean(0) - d).T)).T
+
+    return Y
