8f87e93f970356a83d01f6f30d8acf22ad808dff
Emanuele Olivetti <emanuele.olivetti@gmail.com>
2018-05-18 15:17:04 +0200

    changed correlation_distance() into correlation() + added regularization for distortion()

--- evaluation_metrics.py
+++ evaluation_metrics.py
@@ -11,11 +11,11 @@ def stress(dist_embedd, dist_original):
     return (tmp * tmp).sum()
 
 
-def correlation_distance(dist_embedd, dist_original):
-    return 1 - abs(np.corrcoef(dist_embedd, dist_original)[1,0])
+def correlation(dist_embedd, dist_original):
+    return np.corrcoef(dist_embedd, dist_original)[1,0]
 
 
-def distortion(dist_embedd, dist_original):
+def distortion(dist_embedd, dist_original, eps=1.0e-20):
     """Distortion (c1*c2) of the embedded distances with respect to the
     original distances, where
        (1/c1)*do(o1, o2) <= de(f(o1), f(o2)) <= c2*do(o1,o2)
@@ -24,8 +24,8 @@ def distortion(dist_embedd, dist_original):
     See: Samet, "Foundations of Multidimensional and Metric Data
     Structures", Sec.4.7.1.
     """
-    c1 = np.max([(dist_original / dist_embedd).max(), 1.0])
-    c2 = np.max([(dist_embedd / dist_original).max(), 1.0])
+    c1 = np.max([(dist_original / (dist_embedd + eps)).max(), 1.0])
+    c2 = np.max([(dist_embedd / (dist_original + eps)).max(), 1.0])
     return c1 * c2
 
 
@@ -33,6 +33,6 @@ if __name__ == '__main__':
     dist_original = np.arange(1, 20)
     dist_embedd = dist_original ** 2 + np.random.normal(len(dist_original)) * 0.5
     print("stress = %s" % stress(dist_embedd, dist_original))
-    print("correlation_distance = %s" % correlation_distance(dist_embedd, dist_original))
+    print("correlation = %s" % correlation_distance(dist_embedd, dist_original))
     print("distortion = %s" % distortion(dist_embedd, dist_original))
     
