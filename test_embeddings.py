66bf8490cbe1fe68b28d9ffe7f4347f6b2130e82
Emanuele Olivetti <emanuele.olivetti@gmail.com>
2018-05-22 14:53:43 +0200

    updated test_embeddings.py to new signatures of funcs + minor fixes

--- test_embeddings.py
+++ test_embeddings.py
@@ -3,7 +3,7 @@
 
 import numpy as np
 from dissimilarity import compute_dissimilarity
-from fastmap import fastmap
+from fastmap import compute_fastmap
 from lmds import compute_lmds
 from scipy.spatial import distance_matrix
 from evaluation_metrics import stress, correlation, distortion
@@ -12,7 +12,7 @@ from functools import partial
 from time import time
 
 
-def print_evaluation(X, Y, subsample_size=500):
+def print_evaluation(X, distance, Y, subsample_size=500):
     idx = np.random.permutation(len(X))[:subsample_size]
     D_sub = distance(X[idx], X[idx])
     DY_sub = distance_matrix(Y[idx], Y[idx])
@@ -20,7 +20,7 @@ def print_evaluation(X, Y, subsample_size=500):
     print("  Correlation = %s" % correlation(D_sub.flatten(), DY_sub.flatten()))
     print("  Distortion = %s" % distortion(D_sub.flatten(), DY_sub.flatten()))
 
-    
+
 if __name__ == '__main__':
     print(__doc__)
     np.random.seed(0)
@@ -33,31 +33,28 @@ if __name__ == '__main__':
     print("Estimating the time and quality of embedded distances vs. original distances.")
 
     print("Dissimilarity Representation:")
-    distance = euclidean_distance
-    # distance = euclidean_distance_parallel
+    # distance = euclidean_distance
+    distance = euclidean_distance_parallel
     t0 = time()
-    Y_dissimilarity, prototype_idx = compute_dissimilarity(X, num_prototypes=k,
-                                                           distance=distance,
-                                                           prototype_policy='sff',
-                                                           verbose=False)
+    Y_dissimilarity, prototype_idx = compute_dissimilarity(X, distance, k)
     print("%s sec." % (time() - t0))
-    print_evaluation(X, Y_dissimilarity)
+    print_evaluation(X, distance, Y_dissimilarity)
 
     print("")
     print("Fastmap:")
-    distance = euclidean_distance
-    # distance = euclidean_distance_parallel
+    # distance = euclidean_distance
+    distance = euclidean_distance_parallel
     t0 = time()
-    Y_fastmap = fastmap(X, distance, k)
+    Y_fastmap = compute_fastmap(X, distance, k)
     print("%s sec." % (time() - t0))
-    print_evaluation(X, Y_fastmap)
+    print_evaluation(X, distance, Y_fastmap)
 
     print("")
     print("lMDS:")
-    distance = euclidean_distance
-    # distance = euclidean_distance_parallel
+    # distance = euclidean_distance
+    distance = euclidean_distance_parallel
     t0 = time()
-    Y_lmds = np.array(compute_lmds(X, nl=100, k=k, distance=distance,
-                                   landmark_policy='sff'))
+    Y_lmds = np.array(compute_lmds(X, distance, k, nl=100,
+                                   landmark_policy='random'))
     print("%s sec." % (time() - t0))
-    print_evaluation(X, Y_lmds)
+    print_evaluation(X, distance, Y_lmds)
