c6b9ba26fd9b3ff43399fcb6e48ab74de13b5baa
Emanuele Olivetti <emanuele.olivetti@gmail.com>
2018-05-22 14:25:34 +0200

    removed useless code after refactoring + minor change to docstrings

--- dissimilarity.py
+++ dissimilarity.py
@@ -1,25 +1,18 @@
-"""Computation of the dissimilarity representation of a set of objects
-from a set of prototypes given a distance function. Some prototype
-selection algorithms are available.
+"""Computation of the dissimilarity representation of a dataset of
+objects from a set of prototypes (landmarks( given a distance
+function.
 
 See Olivetti E., Nguyen T.B., Garyfallidis, E., The Approximation of
 the Dissimilarity Projection, http://dx.doi.org/10.1109/PRNI.2012.13
-
 """
 
 from __future__ import division
 import numpy as np
 from subsampling import compute_subset
-try:
-    from joblib import Parallel, delayed, cpu_count
-    joblib_available = True
-except:
-    joblib_available = False
 
 
-def compute_dissimilarity(tracks, num_prototypes=40, distance=None,
-                          prototype_policy='sff', n_jobs=-1,
-                          verbose=False):
+def compute_dissimilarity(dataset, num_prototypes=40, distance=None,
+                          prototype_policy='sff', verbose=False):
     """Compute the dissimilarity (distance) matrix between tracks and
     prototypes, where prototypes are selected among the tracks with a
     given policy.
@@ -36,11 +29,7 @@ def compute_dissimilarity(tracks, num_prototypes=40, distance=None,
            default is bundles_distances_mam
     prototype_policy : string
            Shortname for the prototype selection policy. The default
-           value is 'sff'.
-    n_jobs : int
-           If joblib is available, split the dissimilarity computation
-           in n_jobs. If n_jobs is -1, then all available cpus/cores
-           are used. The default value is -1.
+           value is 'sff', which is highly scalable.
     verbose : bool
            If true prints some messages. Deafault is True.
 
@@ -61,6 +50,5 @@ def compute_dissimilarity(tracks, num_prototypes=40, distance=None,
     prototype_idx = compute_subset(tracks, num_prototypes, distance,
                                    landmark_policy=prototype_policy)
     prototypes = [tracks[i] for i in prototype_idx]
-    dissimilarity_matrix = distance(tracks, prototypes, n_jobs=n_jobs,
-                                    verbose=verbose)
+    dissimilarity_matrix = distance(tracks, prototypes)
     return dissimilarity_matrix, prototype_idx
