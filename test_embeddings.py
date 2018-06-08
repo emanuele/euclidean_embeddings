989de1d00359d948fd7675206b99e992e2924c0e
Emanuele Olivetti <emanuele.olivetti@gmail.com>
2018-06-08 15:29:27 +0200

    added test for Lipschitz embedding

--- test_embeddings.py
+++ test_embeddings.py
@@ -2,6 +2,7 @@
 """
 
 import numpy as np
+from lipschitz import compute_lipschitz
 from dissimilarity import compute_dissimilarity
 from fastmap import compute_fastmap
 from lmds import compute_lmds
@@ -32,9 +33,19 @@ if __name__ == '__main__':
     
     print("Estimating the time and quality of embedded distances vs. original distances.")
 
+    print("")
+    print("Lipschitz embedding:")
+    distance = euclidean_distance
+    # distance = euclidean_distance_parallel
+    t0 = time()
+    Y_dissimilarity, R = compute_lipschitz(X, distance, k)
+    print("%s sec." % (time() - t0))
+    print_evaluation(X, distance, Y_dissimilarity)
+
+    print("")
     print("Dissimilarity Representation:")
-    # distance = euclidean_distance
-    distance = euclidean_distance_parallel
+    distance = euclidean_distance
+    # distance = euclidean_distance_parallel
     t0 = time()
     Y_dissimilarity, prototype_idx = compute_dissimilarity(X, distance, k)
     print("%s sec." % (time() - t0))
@@ -42,8 +53,8 @@ if __name__ == '__main__':
 
     print("")
     print("Fastmap:")
-    # distance = euclidean_distance
-    distance = euclidean_distance_parallel
+    distance = euclidean_distance
+    # distance = euclidean_distance_parallel
     t0 = time()
     Y_fastmap = compute_fastmap(X, distance, k)
     print("%s sec." % (time() - t0))
@@ -51,8 +62,8 @@ if __name__ == '__main__':
 
     print("")
     print("lMDS:")
-    # distance = euclidean_distance
-    distance = euclidean_distance_parallel
+    distance = euclidean_distance
+    # distance = euclidean_distance_parallel
     t0 = time()
     Y_lmds = np.array(compute_lmds(X, distance, k, nl=100,
                                    landmark_policy='random'))
